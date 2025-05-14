70fe9f64074a620e06683dde76b448aed46bf6ef
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2025-05-14 07:19:39 -0400

    Use factoryBot to write unit test for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,19 +4,19 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   describe 'validations' do
     it 'is valid with a unique name' do
-      category = Category.new(name: 'Furniture')
+      category = build(:category, name: "Furniture")
       expect(category).to be_valid
     end
 
     it 'is invalid without a name' do
-      category = Category.new(name: nil)
+      category = build(:category, name: nil)
       expect(category).not_to be_valid
       expect(category.errors[:name]).to include("can't be blank")
     end
 
     it 'is invalid with a duplicate name' do
-      Category.create!(name: 'Electronics')
-      duplicate_category = Category.new(name: 'Electronics')
+      create(:category, name: 'Electronics')
+      duplicate_category = build(:category, name: 'Electronics')
       expect(duplicate_category).not_to be_valid
       expect(duplicate_category.errors[:name]).to include('has already been taken')
     end
