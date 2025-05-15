37562309f313e1359f4f371d6b0473e066cd6f09
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2025-05-15 05:49:13 -0400

    Add category feature for report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,23 @@
+
 class ReportsController < ApplicationController
-def index
-  if params[:start_date].present? && params[:end_date].present?
-    @start_date = Date.parse(params[:start_date])
-    @end_date = Date.parse(params[:end_date])
-    @stock_ins = InventoryLog.where(operation: "stock_in", created_at: @start_date.beginning_of_day..@end_date.end_of_day)
-    @stock_outs = InventoryLog.where(operation: "stock_out", created_at: @start_date.beginning_of_day..@end_date.end_of_day)
-  else
-    @stock_ins = []
-    @stock_outs = []
+  def index
+    @categories = Category.all
+
+    if params[:start_date].present? && params[:end_date].present?
+      @start_date = Date.parse(params[:start_date])
+      @end_date = Date.parse(params[:end_date])
+      logs_scope = InventoryLog.where(created_at: @start_date.beginning_of_day..@end_date.end_of_day)
+
+      if params[:category_id].present?
+        logs_scope = logs_scope.joins(:product).where(products: { category_id: params[:category_id] })
+        @selected_category = Category.find_by(id: params[:category_id])
+      end
+
+      @stock_ins = logs_scope.where(operation: "stock_in")
+      @stock_outs = logs_scope.where(operation: "stock_out")
+    else
+      @stock_ins = []
+      @stock_outs = []
+    end
   end
-end
 end
