7429b6599f80b60cf509f591161bf9dd5cbf44dd
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2025-05-26 02:50:22 -0400

    Remove an error of CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
 
   include Pundit::Authorization
 
-  # Rescue from unauthorized access
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
-
 
   def user_not_authorized
     flash[:alert] = "Access denied."
